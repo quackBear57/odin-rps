1d466fa719198d731632bd4742a8511522aadf6d
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-04 09:00:11 -0600

    intialize getHumanChoice function

--- rps.js
+++ rps.js
@@ -13,4 +13,21 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function getHumanChoice() {
+    let input = parseInt(prompt("Enter 1 for Paper, 2 for Rock, or 3 for Scissors: "));
+    // console.log(input);
+    let humanChoice = "TBD";
+    if (input == 1) {
+        humanChoice = "Rock";
+    } else if (input == 2) {
+        humanChoice = "Paper";
+    } else if (input == 3) {
+        humanChoice = "Scissors";
+    } else {
+        console.log("Invalid input (not 1, 2 or 3).");
+    }
+    return humanChoice;
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
